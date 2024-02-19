ac249a6f55cbd5be1939e06ec601660510e06e8b
Nelson Musto <mustonelson@gmail.com>
2024-02-19 16:33:26 -0300

    Structure: Added Lombok

--- src/main/java/com/application/rest/SpringBootRest/entities/Client.java
+++ src/main/java/com/application/rest/SpringBootRest/entities/Client.java
@@ -1,9 +1,14 @@
 package com.application.rest.SpringBootRest.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 
 @Entity
+@Getter @Setter @ToString
 @Table(name = "client")
 public class Client implements Serializable {
 
@@ -24,37 +29,8 @@ public class Client implements Serializable {
         this.dni = dni;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-    public String getLastname() {
-        return this.lastname;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
     public Client() {
     }
 
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", dni=" + dni +
-                '}';
-    }
+
 }
--- src/main/java/com/application/rest/SpringBootRest/services/ClientService.java
+++ src/main/java/com/application/rest/SpringBootRest/services/ClientService.java
@@ -1,10 +1,23 @@
 package com.application.rest.SpringBootRest.services;
 
 import com.application.rest.SpringBootRest.entities.Client;
+import com.application.rest.SpringBootRest.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ClientService implements BaseService<Client> {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public ClientService (ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+
     @Override
     public List<Client> findAll() throws Exception {
         return null;
