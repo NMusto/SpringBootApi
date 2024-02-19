393989729b12b47046f823b083c4f3639b77d91a
Nelson Musto <mustonelson@gmail.com>
2024-02-19 17:46:03 -0300

    Structure: ClientService

--- src/main/java/com/application/rest/SpringBootRest/services/ClientService.java
+++ src/main/java/com/application/rest/SpringBootRest/services/ClientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService implements BaseService<Client> {
@@ -20,26 +21,60 @@ public class ClientService implements BaseService<Client> {
 
     @Override
     public List<Client> findAll() throws Exception {
-        return null;
+        try {
+            List<Client> clients = clientRepository.findAll();
+            return clients;
+        }
+        catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
     public Client findById(Long id) throws Exception {
-        return null;
+        try {
+            Optional<Client> clientOptional = clientRepository.findById(id);
+            return clientOptional.get();
+        }
+        catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
     public Client save(Client entity) throws Exception {
-        return null;
+        try {
+            Client client = clientRepository.save(entity);
+            return client;
+        }
+        catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
     public Client update(Long id, Client entity) throws Exception {
-        return null;
+        try {
+            Optional<Client> clientOptional = clientRepository.findById(id);
+            Client client = clientOptional.get();
+            client = clientRepository.save(client);
+            return client;
+        }
+        catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
     public boolean delete(Long id) throws Exception {
-        return false;
+        try {
+            if (clientRepository.existsById(id)) {
+                clientRepository.deleteById(id);
+                return true;
+            } else throw new Exception();
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
